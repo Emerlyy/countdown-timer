28293d3098828a1e44ef5c41eef83b90fab7b1e8
Emerlyy <vadim200339@gmail.com>
2023-03-02 12:54:49 +0200

    created Subtitle for the card

---
+++ src/components/Subtitle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Subtitle = styled.h3`
+  margin: 25px 0 0 0;
+  padding: 0;
+  color: var(--colors-timer-subtitle);
+  text-transform: uppercase;
+  text-align: center;
+  font-size: .7rem;
+  letter-spacing: .4rem;
+`
