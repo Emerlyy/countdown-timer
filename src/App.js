0b977bb75f0fb1ac8a53a6c1498ce6ad24eaec1a
Emerlyy <vadim200339@gmail.com>
2023-03-02 13:01:50 +0200

    created main logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import styled from 'styled-components';
+import { Footer } from './components/Footer';
+import { Timer } from './components/Timer';
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-image: url('/images/bg-stars.svg'), var(--colors-bg);
+  height: 100vh;
+`
+
+const Main = styled.section`
+  flex-grow: 1;
+  padding: 120px 0 0 0;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-transform: uppercase;
+  text-align: center;
+  color: var(--colors-white);
+  font-weight: var(--fw-bold);
+  font-size: 1.1rem;
+  line-height: 1rem;
+  letter-spacing: .4rem;
+`
 
 function App() {
+  const date = new Date().setHours(new Date().getHours() + 336);
+
   return (
-    <div className="App">
-     
-    </div>
+    <Body>
+      <Main>
+        <Title>We're launching soon</Title>
+        <Timer countToDate={date} />
+      </Main>
+      <Footer />
+    </Body >
   );
 }
 
