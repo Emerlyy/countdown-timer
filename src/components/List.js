22ccc616f3bf1e3d0c5e7596bfdae71f53765264
Emerlyy <vadim200339@gmail.com>
2023-03-02 12:54:33 +0200

    created List

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display:flex;
+  padding: 0;
+  margin: 20px 0 0 0;
+  align-items- center;
+  column-gap: 25px;
+  list-style-type:none;
+`;
